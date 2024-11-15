737d08c7811a68ce144696316d7059d845382b92
kondurilaxmiprasanna <kondurilaxmiprasanna72@gmail.com>
2024-11-15 16:57:02 +0530

    Create RPSsubmission.py

---
+++ visaa_prep/RPSsubmission.py
@@ -0,0 +1,16 @@
+def determine_winner(vignesh, charan):
+    beats = {
+        'R': 'S',
+        'S': 'P',
+        'P': 'R'
+    }
+    
+    if vignesh == charan:
+        return "NULL"
+    elif beats[vignesh] == charan:
+        return "Vignesh"
+    else:
+        return "Charan"
+vignesh, charan = input().split()
+
+print(determine_winner(vignesh.upper(), charan.upper()))
